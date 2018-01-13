2d7183169dab81d180aef85a062fb10ba5b93e4d
Berrou Glenn <gberrou@barn-e-01.enssat.fr>
2018-01-13 15:07:48 +0100

    new dataset 1800< class frequency <500

--- mail2classes.py
+++ mail2classes.py
@@ -1,43 +1,39 @@
 import pandas as pd
 import numpy as np
-import nltk
-import matplotlib.pyplot as plt
+#import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
-from nltk import word_tokenize          
-from nltk.stem import WordNetLemmatizer 
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.stem.porter import PorterStemmer
-
+from sklearn.naive_bayes import GaussianNB
 # nltk.download('punkt')
 # nltk.download('wordnet')
-stemmer = PorterStemmer()
+#stemmer = PorterStemmer()
 
-def stem_tokens(tokens, stemmer):
-    stemmed = []
-    for item in tokens:
-        stemmed.append(stemmer.stem(item))
-    return stemmed
+#def stem_tokens(tokens, stemmer):
+ #   stemmed = []
+  #  for item in tokens:
+   #     stemmed.append(stemmer.stem(item))
+    #return stemmed
 
-def tokenize(text):
-    tokens = nltk.word_tokenize(text)
-    stems = stem_tokens(tokens, stemmer)
-    return stems
+#def tokenize(text):
+ #   tokens = nltk.word_tokenize(text)
+  #  stems = stem_tokens(tokens, stemmer)
+   # return stems
 
-df = pd.read_csv('americasOrCalendar.csv', na_values=['?'],header=0)
+df = pd.read_csv('../shorted_email_1800_500.csv', na_values=['?'],header=0)
 
-df["all"] = df["X-To"].map(str) + df["content"].map(str)
+df["all"] = df["X-From"].map(str)
 
 X=df['all']
 # print(X)
 # X=df[['content', 'Subject']]
 y=df['class']
 
-vectorizer = TfidfVectorizer(tokenizer=tokenize,sublinear_tf=True,stop_words='english')
-# vectorizer = TfidfVectorizer(sublinear_tf=True,stop_words='english')
+
+#vectorizer = TfidfVectorizer(tokenizer=tokenize,sublinear_tf=True,stop_words='english')
+vectorizer = TfidfVectorizer(sublinear_tf=True,stop_words='english')
 X = vectorizer.fit_transform(X)
+#clf = GaussianNB()
 # # print (X)
 clf = DecisionTreeClassifier(random_state=0)
-print(np.mean(cross_val_score(clf, X, y, cv=10)))
+print(np.mean(cross_val_score(clf,X, y, cv=10)))
--- preprocessing.py
+++ preprocessing.py
@@ -9,8 +9,8 @@ import os,sys, email, re
 import pandas as pd
 
 
-input_path = './emails.csv'
-output_path = './shorted_emails.csv'
+input_path = '../emails.csv'
+output_path = '../shorted_emails.csv'
 # Read the data into a DataFrame
 emails_df = pd.read_csv(input_path)
 print(emails_df.shape)   
@@ -61,5 +61,5 @@ print("Set index and drop columns with two few values")
 
 emails_df['class'].value_counts()
 
-# shorted_emails_df = emails_df.head(10000)
-# shorted_emails_df.to_csv(output_path,sep=',')
+emails_df = emails_df.head(350000)
+emails_df.to_csv(output_path,sep=',')

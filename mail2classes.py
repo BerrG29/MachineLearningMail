1c6441be8ffc09e74db47337d05d9dd3aa7844e6
berrou <glennberr29@gmail.com>
2018-01-15 09:29:55 +0100

    neural network test

--- mail2classes.py
+++ mail2classes.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.neural_network import MLPClassifier
 #import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import cross_val_score
@@ -20,20 +21,26 @@ from sklearn.naive_bayes import GaussianNB
   #  stems = stem_tokens(tokens, stemmer)
    # return stems
 
+#######################DATASET##################################################
 df = pd.read_csv('../shorted_email_1800_500.csv', na_values=['?'],header=0)
-
-df["all"] = df["X-From"].map(str)
-
-X=df['all']
-# print(X)
-# X=df[['content', 'Subject']]
+df["features"] = df["X-From"].map(str)
+X=df['features']
 y=df['class']
 
-
+########################DESCRIPTORS##############################################
 #vectorizer = TfidfVectorizer(tokenizer=tokenize,sublinear_tf=True,stop_words='english')
 vectorizer = TfidfVectorizer(sublinear_tf=True,stop_words='english')
 X = vectorizer.fit_transform(X)
+
+########################MODEL###################################################
+##Naive bayes Gaussian
 #clf = GaussianNB()
-# # print (X)
-clf = DecisionTreeClassifier(random_state=0)
+##Decision tree
+#clf = DecisionTreeClassifier(random_state=0)
+##Neural network
+parameters = {'hidden_layer_sizes ': [(100,),(100,100), (100,100,100)], 'alpha':[10.0 ** -np.arange(1, 7)], 'activation':('identity', 'logistic', 'tanh', 'relu'), 'solver' :('lbfgs', 'sgd', 'adam'), 'learning_rate' : ('constant', 'invscaling', 'adaptive'), 'learning_rate_init':[0.05, 0.01, 0.005, 0.001], 'power_t':[0.1, 0.5, 1, 2]}
+nn=neural_network.MLPClassifier()
+clf = GridSearchCV(nn, parameters)
+
+#######################RESULTS##################################################
 print(np.mean(cross_val_score(clf,X, y, cv=10)))

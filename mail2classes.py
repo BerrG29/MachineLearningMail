090064436df6078fc04f9d0ab5f8536b926bb83f
chaka <ngamenichaka@gmail.com>
2018-01-15 15:00:19 +0100

    gridsearch sur decision tree

--- mail2classes.py
+++ mail2classes.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.grid_search import GridSearchCV
 # nltk.download('punkt')
 # nltk.download('wordnet')
 #stemmer = PorterStemmer()
@@ -22,6 +23,7 @@ from sklearn.naive_bayes import GaussianNB
    # return stems
 
 #######################DATASET##################################################
+
 df = pd.read_csv('../shorted_email_1800_500.csv', na_values=['?'],header=0)
 df["features"] = df["X-From"].map(str)
 X=df['features']
@@ -36,11 +38,22 @@ X = vectorizer.fit_transform(X)
 ##Naive bayes Gaussian
 #clf = GaussianNB()
 ##Decision tree
-#clf = DecisionTreeClassifier(random_state=0)
+parameters = {
+                'criterion': ['gini','entropy'], 
+                'splitter': ['best','random']
+         }
+dtc = DecisionTreeClassifier(random_state=0)
+grid = GridSearchCV(dtc, parameters)
+#
 ##Neural network
 parameters = {'hidden_layer_sizes ': [(100,),(100,100), (100,100,100)], 'alpha':[10.0 ** -np.arange(1, 7)], 'activation':('identity', 'logistic', 'tanh', 'relu'), 'solver' :('lbfgs', 'sgd', 'adam'), 'learning_rate' : ('constant', 'invscaling', 'adaptive'), 'learning_rate_init':[0.05, 0.01, 0.005, 0.001], 'power_t':[0.1, 0.5, 1, 2]}
 nn=neural_network.MLPClassifier()
-clf = GridSearchCV(nn, parameters)
+grid = GridSearchCV(nn, parameters)
 
 #######################RESULTS##################################################
 print(np.mean(cross_val_score(clf,X, y, cv=10)))
+
+print("The best model : ")
+print("- best score  : {}\n".format(grid.best_score_))
+print("- best params : {}\n".format(grid.best_params_))
+print("- best estimator : {}\n".format(grid.best_estimator_))

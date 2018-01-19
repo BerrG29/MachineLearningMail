c2851afa2cb8c3dcc683d7d4bcb16087f18ee15b
Berrou Glenn <gberrou@barn-e-01.enssat.fr>
2018-01-19 19:55:58 +0100

    Chacka à toi

--- mail2classes.py
+++ mail2classes.py
@@ -6,10 +6,10 @@ from sklearn.grid_search import GridSearchCV
 import imports
 from imports import descriptors, model
 
-#dataSets=['../kaminiski_unbalanced_600_50.csv','../all_balanced.csv','../all_unbalanced_100.csv','../kaminski_unbalanced_50.csv', '../all_multiplePerson.csv','../all_unbalanced_2600_1000.csv']
-dataSets=['../kaminiski_unbalanced_600_50.csv']
+#dataSets=['../kaminski_unbalanced_600_50.csv','../all_balanced_100_predictable.csv','../kaminski_balanced_50.csv', '../all_multiplePerson.csv','../all_unbalanced_2600_1000.csv']
+dataSets=['../kaminski_unbalanced_600_50.csv']
 fieldToTest=['Content','X-From']
-descriptors_list=['frequence']
+descriptors_list=['tf_idf','frequence']
 models_list=['neural_network']
 
 for dataSet in dataSets:
@@ -28,20 +28,19 @@ for dataSet in dataSets:
         
         best_grid = {}
         current_grid = {}
-        
+
         ########################INITIALISATION############################################
         best_descriptor = "tf_idf"
         best_model = "decision_tree"
         X=descriptors.tf_idf(X)
         best_grid = model.decision_tree(X,y)
-        
-            
+
         ########################DESCRIPTORS############################################
         for descriptor in descriptors_list:
-            current_descriptor = descriptor
-            
             if(descriptor == "frequence"):
                 X=descriptors.frequence(X)
+            if(descriptor == "tf_idf"):
+                X=descriptors.tf_idf(X)
 
             #######################MODELS##################################################
             for m in models_list:
